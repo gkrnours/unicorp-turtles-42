2bf924c679775fff890455b48a93b38d67f7813d
l. coues <couesl@gmail.com>
2026-09-24 19:33:47 +0200

    Add FPS counter

--- src/unicorp_turtles_42/gui.py
+++ src/unicorp_turtles_42/gui.py
@@ -3,6 +3,51 @@ from pyglet.math import Vec2
 
 from unicorp_turtles_42.loader import colors, loader
 
+BOX_COLORS = {
+    "bottom": "navy",
+    "middle": "cerulean",
+    "top": "ochre",
+}
+
+
+def box(repeat, screen, batch, group):
+    images_repeat = [
+        (loader().image(f"gfx/UI-repeat-{part}.png"), BOX_COLORS[part])
+        for part in ["bottom", "middle", "top"]
+    ]
+    images_end = [
+        (loader().image(f"gfx/UI-end-{part}.png"), BOX_COLORS[part])
+        for part in ["bottom", "middle", "top"]
+    ]
+    delta = images_repeat[0][0].width
+
+    parts = []
+    sprites_config = dict(
+        batch=batch,
+        group=group,
+    )
+    for i in range(repeat):
+        for img, color in images_repeat:
+            sprite = pyglet.sprite.Sprite(
+                img=img,
+                y=screen.height - img.height,
+                x=0 + delta * i,
+                **sprites_config,
+            )
+            sprite.color = colors()[color]
+            parts.append(sprite)
+    for img, color in images_end:
+        sprite = pyglet.sprite.Sprite(
+            img=img,
+            y=screen.height - img.height,
+            x=0 + delta * repeat,
+            **sprites_config,
+        )
+        sprite.color = colors()[color]
+        parts.append(sprite)
+
+    return parts
+
 
 class GUI(pyglet.event.EventDispatcher):
     def __init__(self, screen):
@@ -13,6 +58,7 @@ class GUI(pyglet.event.EventDispatcher):
         self._drawing_batch = None
         self._layers = None
         self._sprites = None
+        self._fps = None
 
         self._build()
 
@@ -20,29 +66,16 @@ class GUI(pyglet.event.EventDispatcher):
         self._drawing_batch = batch = pyglet.graphics.Batch()
         group = pyglet.graphics.Group(order=100)
 
-        layer_images = [
-            (loader().image(f"gfx/UI-end-{part}.png"), color)
-            for part, color in [
-                ("bottom", "navy"),
-                ("middle", "cerulean"),
-                ("top", "ochre"),
-            ]
-        ]
+        self._layers = box(1, self._screen, batch, group)
 
-        self._layers = []
-        for img, color in layer_images:
-            sprite = pyglet.sprite.Sprite(
-                img=img,
-                x=0,
-                y=self.height - img.height,
-                batch=batch,
-                group=group,
-            )
-            sprite.color = colors()[color]
-            self._layers.append(sprite)
+        self._fps = pyglet.window.FPSDisplay(self._screen)
+        self._fps.label.anchor_y = "top"
+        self._fps.label.x = 0
+        self._fps.label.y = self.height
 
     def draw(self):
         self._drawing_batch.draw()
+        self._fps.draw()
 
     def update(self, dt):
         pass
